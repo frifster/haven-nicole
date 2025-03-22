a43793a81281de3f48e376aa357201cab2f46ecf
Eugene Avila <eugene@comfactechoptions.com>
2025-03-22 20:11:35 +0800

    Add routing for Contact page in App component to improve site navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import About from './pages/About';
 import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
 import Bundle from './pages/Bundle';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
 import HowItWorks from './pages/HowItWorks';
 import Product from './pages/Product';
@@ -27,6 +28,7 @@ const App: React.FC = () => {
             <Route path="/bundle" element={<Bundle />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/:postId" element={<BlogPost />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
